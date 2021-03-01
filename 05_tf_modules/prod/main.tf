d0234c584fc48419358b5c9ff5241ad94a928233
Ganesh <ganesh@fixmygadgets.in>
2021-03-01 19:10:20 +0100

    Init

---
+++ 05_tf_modules/prod/main.tf
@@ -0,0 +1,37 @@
+// Variable declaration
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "eu-central-1"
+}
+variable "env" {
+  description = "Environment variable"
+  default     = "test"
+}
+
+variable "profile" {
+  description = "Profile for shared credentials"
+  default     = "default"
+}
+
+
+provider "aws" {
+  region                    = var.aws_region
+  shared_credentials_file   = "%USERPROFILE%/.aws/credentials"
+  profile                   = var.profile
+}
+
+  module "my_vpc" {
+    source      = "../modules/vpc"
+    vpc_cidr    = "192.168.0.0/16"
+    tenancy     = "default"
+    vpc_id      = module.my_vpc.vpc_id
+    subnet_cidr = "192.168.1.0/24"
+  }
+  
+  module "my_ec2" {
+    source        = "../modules/ec2"
+    ec2_count     = 1
+    ami_id        = "ami-0932440befd74cdba"
+    instance_type = "t2.micro"
+    subnet_id     = module.my_vpc.subnet_id
+  }
